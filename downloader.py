6b802ef8c3a4da24adcaaf3d930534e8b24edb26
Aamir Mirza <aamirshahed@hotmail.com>
2021-01-07 13:12:40 -0500

    Created downloader abstract base class.

---
+++ downloader.py
@@ -0,0 +1,23 @@
+import os
+from abc import ABC, abstractmethod
+
+DOWNLOAD_FOLDER = os.path.dirname(__file__) + '/downloads'
+
+
+class Downloader(ABC):
+    def __init__(self, download_folder=None):
+        if download_folder:
+            self.download_folder = download_folder
+        else:
+            self.download_folder = DOWNLOAD_FOLDER
+
+    @abstractmethod
+    def download(self, url, start_at_position=1):
+        """Download the video and return what position you downloaded up to."""
+        pass
+
+    @abstractmethod
+    def get_info(self):
+        """Get information on what's to be downloaded, such as links,
+        numeber of files etc."""
+        pass
