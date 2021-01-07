c8baaafdeb2a37395fa204075815fc9f29b6be74
Aamir Mirza <aamirshahed@hotmail.com>
2021-01-07 18:06:21 -0500

    Wrote pseudocode for DownloadManager class

---
+++ download_manager.py
@@ -0,0 +1,19 @@
+"""
+DownloadManager is a class that takes a Downloader objects, retains knowledge
+of which files have already been downloaded, and which files are new and
+should be downloaded.
+"""
+
+# Downloader class
+    # Init
+        # An iteration of Downloader objects
+
+    # Download All
+        # For service in downloader:
+            # Download files starting at a position
+            # Save 'position' of the last file downloaded Save_position()
+
+    # Save position
+        # Saves last file downloaded, to know where to start from afterwards
+
+    
