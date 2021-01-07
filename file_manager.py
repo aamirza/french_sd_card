44f80446f2fcac6708d0d2c0086f570ce27ab481
Aamir Mirza <aamirshahed@hotmail.com>
2021-01-07 18:14:17 -0500

    Wrote pseudocode for file_manager.py

---
+++ file_manager.py
@@ -0,0 +1,15 @@
+"""
+File manager manages migrating files from the download
+folder onto an SD card or some other storage.
+"""
+
+LOCAL_DOWNLOAD_FOLDER = "Directory to download folder on local storage"
+EXTERNAL_DOWNLOAD_FOLDER = "Directory to download folder on external storage"
+
+# Function move files
+
+# Function delete files
+
+# Function move to external storage
+
+# Function delete from local storage
