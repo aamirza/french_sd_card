5f862e2e4e6b85e595555a01e73ecfed683e28ec
Aamir Mirza <aamirshahed@hotmail.com>
2021-01-10 23:31:45 -0500

    Move file to SD card function

--- file_manager.py
+++ file_manager.py
@@ -2,6 +2,7 @@
 File manager manages migrating files from the download
 folder onto an SD card or some other storage.
 """
+import os
 
 LOCAL_DOWNLOAD_FOLDER = "Directory to download folder on local storage"
 EXTERNAL_DOWNLOAD_FOLDER = "Directory to download folder on external storage"
@@ -11,5 +12,8 @@ EXTERNAL_DOWNLOAD_FOLDER = "Directory to download folder on external storage"
 # Function delete files
 
 # Function move to external storage
+
+def move_file_to_external_storage(file):
+    os.rename(file, EXTERNAL_DOWNLOAD_FOLDER)
 
 # Function delete from local storage
