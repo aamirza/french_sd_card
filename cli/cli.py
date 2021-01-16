785ab0408e08e39d9835bf36b9861bc9c6116648
Aamir Mirza <aamirshahed@hotmail.com>
2021-01-16 18:01:54 -0500

    Wrote pseudocode for the CLI

---
+++ cli/cli.py
@@ -0,0 +1,12 @@
+"""
+Command line interface for the program.
+"""
+
+# Import argparse
+# ArgumentParser class
+    # "Add"
+        # "Service" (e.g. youtube)
+    # "Remove"
+        # URL
+    # Download
+# Build parse
