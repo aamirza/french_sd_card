de25487e8c29b8ff1c46c3dd954cf6f68beda2dd
Aamir Mirza <aamirshahed@hotmail.com>
2021-01-16 16:58:06 -0500

    Use RaiseRegex instead of RaiseRegexp

--- tests/test_file_manager.py
+++ tests/test_file_manager.py
@@ -10,8 +10,8 @@ class MyTestCase(unittest.TestCase):
         self.fm = FileManager()
 
     def test_fileManager_invalidDirectoryRaisesError(self):
-        with self.assertRaisesRegexp(file_manager.InvalidDirectoryError,
-                                     " is not a valid directory"):
+        with self.assertRaisesRegex(file_manager.InvalidDirectoryError,
+                                    " is not a valid directory"):
             fm = FileManager(local_download_folder="lalala/download")
 
     @mock.patch.object(file_manager.os.path, 'isdir')
