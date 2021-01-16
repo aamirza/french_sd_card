27d0f4947e8ec5733befdeb98254db207b632937
Aamir Mirza <aamirshahed@hotmail.com>
2021-01-16 16:11:22 -0500

    Removed pseudocode

--- download_manager.py
+++ download_manager.py
@@ -3,6 +3,7 @@ DownloadManager is a class that takes a Downloader objects, retains knowledge
 of which files have already been downloaded, and which files are new and
 should be downloaded.
 """
+
 import functools
 import os
 import pickle
@@ -60,17 +61,3 @@ class DownloadManager:
         except ValueError:
             position = 1
         self.download_positions.update({url: position})
-
-# Downloader class
-    # Init
-        # An iteration of Downloader objects
-
-    # Download All
-        # For service in downloader:
-            # Download files starting at a position
-            # Save 'position' of the last file downloaded Save_position()
-
-    # Save position
-        # Saves last file downloaded, to know where to start from afterwards
-
-    
