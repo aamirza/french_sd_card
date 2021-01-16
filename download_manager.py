55f617276e8fe48a3395698bfbb6379b84548b9c
Aamir Mirza <aamirshahed@hotmail.com>
2021-01-15 19:42:42 -0500

    Wrote get_positions function in download manager

--- download_manager.py
+++ download_manager.py
@@ -3,9 +3,12 @@ DownloadManager is a class that takes a Downloader objects, retains knowledge
 of which files have already been downloaded, and which files are new and
 should be downloaded.
 """
+import os
+import pickle
 
 
 class DownloadManager:
+    LAST_POSITIONS_FILE = os.path.dirname(__file__) + "/positions.pk1"
     def __init__(self, *downloaders):
         """
         :param downloaders: Object of type Downloader
@@ -13,9 +16,18 @@ class DownloadManager:
         self.services = [downloader for downloader in downloaders]
 
     def download_all(self):
-        '''Download all new audio from all the services'''
+        """Download all new audio from all the services"""
         for service in self.services:
             service.download()
+
+    def get_positions(self):
+        """Get last download positions for each URL"""
+        try:
+            with open(self.LAST_POSITIONS_FILE, 'rb') as positions_file:
+                positions = pickle.load(positions_file)
+        except FileNotFoundError:
+            positions = {}
+        return positions
 
 # Downloader class
     # Init
--- tests/test_download_manager.py
+++ tests/test_download_manager.py
@@ -9,18 +9,38 @@ from youtube import YoutubeDownloader
 
 class FileManagerTestCase(unittest.TestCase):
     def setUp(self):
-        youtube = YoutubeDownloader()
+        youtube = YoutubeDownloader(self.billboard2019_playlist())
         self.dm = DownloadManager(youtube)
+
+    def billboard2019_playlist(self):
+        '''Sample playlist'''
+        return "https://www.youtube.com/playlist?list=" \
+               "PLCzImqCYRfNfJOzblgIkAPkG0HuMBtT5j"
 
     @mock.patch.object(youtube.YoutubeDownloader, 'download')
     def test_downloadPlaylist_downloadsToFolder(self, mock_download):
         self.dm.download_all()
+
         mock_download.assert_called()
 
-    def test_downloadPlaylist_updatesLastVideoDownloaded(self):
-        self.fail("Once a playlist is downloaded, the index of the last "
-                  "video downloaded should be updated and stored "
-                  "somewhere.")
+    @mock.patch.object(youtube.YoutubeDownloader, 'download')
+    @mock.patch('download_manager.pickle')
+    def test_downloadPlaylist_updatesLastVideoDownloaded(self, mock_pickler,
+                                                         mock_download):
+        self.dm.download_all()
+
+        mock_pickler.assert_called()
+
+    @mock.patch.object(download_manager.pickle, 'load')
+    def test_getPositions_returnsPickleFilePositions(self, mock_pickler):
+        expected_return_value = {self.billboard2019_playlist(): "1"}
+        mock_pickler.return_value = expected_return_value
+
+        with mock.patch('builtins.open'):
+            positions = self.dm.get_positions()
+
+        self.assertEqual(expected_return_value, positions)
+
 
     def test_downloadPlaylist_invalidDownloadFolder_raisesErrors(self):
         self.fail("If a download folder is invalid, an error needs"
