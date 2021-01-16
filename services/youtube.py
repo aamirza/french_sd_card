d1190170b2d302cf42afe80811b40cc75e3c4489
Aamir Mirza <aamirshahed@hotmail.com>
2021-01-16 17:54:58 -0500

    Added comments to Youtube class

--- services/youtube.py
+++ services/youtube.py
@@ -1,8 +1,11 @@
-import os
+"""
+The Youtube class is a "Downloader" (downloader.py) for downloading all the
+audio in a Youtube playlist.
+"""
 
 from youtube_dl import YoutubeDL
 
-from downloader import Downloader
+from services.downloader import Downloader
 
 
 class InvalidPlaylistError(Exception):
@@ -11,8 +14,10 @@ class InvalidPlaylistError(Exception):
 
 
 def raise_error_if_invalid_playlist(func):
-    """Wrapper function for validating a playlist URL"""
+    """Wrapper function for validating a playlist URL. URL should be the first
+    argument passed to the function."""
     def wrapper_raise_error_when_invalid_playlist(self, *args, **kwargs):
+        # URL should be the first argument passed.
         url = args[0]
         if not YoutubeDownloader.valid_playlist(url):
             raise InvalidPlaylistError(f"Playlist URL is invalid: {url}")
@@ -34,15 +39,18 @@ class YoutubeDownloader(Downloader):
 
     @property
     def default_params(self):
+        """"Params for YoutubeDL"""
         return {"forcejson": True, "nocheckcertificate": True,
                 "outputdl": f"{self.download_folder}/%(title)s.%(ext)s"}
 
     @property
     def simulate_download_params(self):
+        """"Params for YoutubeDL"""
         return dict(**self.default_params, simulate=True)
 
     @property
     def download_audio_params(self):
+        """"Params for YoutubeDL"""
         return dict(**self.default_params,
                     postprocessors=[{'key': 'FFmpegExtractAudio',
                                      'preferredcodec': 'mp3',
@@ -51,20 +59,24 @@ class YoutubeDownloader(Downloader):
 
     @staticmethod
     def valid_link(url):
+        """Checks for valid Youtube URL"""
         return url.startswith('https://www.youtube.com/')
 
     @staticmethod
     def valid_playlist(url):
+        """Checks for valid Youtube playlist URL"""
         return url.startswith('https://www.youtube.com/playlist?list=')
 
     @raise_error_if_invalid_playlist
     def get_playlist_videos_info(self, playlist_url):
+        """Return information about each video in the playlist."""
         with YoutubeDL(self.default_params) as youtube_dl:
             info = youtube_dl.extract_info(playlist_url, download=False)
         return info["entries"]
 
     @raise_error_if_invalid_playlist
     def download_playlist(self, url, start_at_video=1, download_audio=True):
+        """Download all the videos (audio) of a Youtube playlist"""
         params = self.download_audio_params if download_audio else self.default_params
         if start_at_video > 1:
             params = dict(**params, playliststart = start_at_video)
