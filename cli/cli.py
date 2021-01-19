73b690937436c4f5e9f1f67316c204cf534ecc1c
Aamir Mirza <aamirshahed@hotmail.com>
2021-01-18 22:35:13 -0500

    Initialized argparse

--- cli/cli.py
+++ cli/cli.py
@@ -1,6 +1,22 @@
 """
 Command line interface for the program.
 """
+import argparse
+
+PROGRAM_NAME = "SD Card File Manager"  # TODO: Think of a more accurate name
+PROGRAM_DESCRIPTION = "Download audio files from a URL. Move later to an SD " \
+                      "card or external storage."
+
+COMMANDS = (
+    "add", "rm", "download"
+)
+
+
+def main():
+    parser = argparse.ArgumentParser(prog=PROGRAM_NAME,
+                                     description=PROGRAM_DESCRIPTION)
+    parser.add_argument("Command", choices=COMMANDS, type=str,
+                        metavar="command", nargs=1, action='store')
 
 # Import argparse
 # ArgumentParser class
